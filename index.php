7384da334d3de4d36cc44c6f5c663449d721961e
giomen88 <giorgia9288@virgilio.it>
2022-09-09 20:42:54 +0200

    Classe, variabili  e costruttore

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php 
 
+class Movie
+{
+public $title;
+public $year;
+public $genre;
+
+    public function __construct($title, $year, $genre)
+    {
+    $this->title = $title;
+    $this->year = $year;
+    $this->genre = $genre;
+    }
+}
+
+
 ?>
